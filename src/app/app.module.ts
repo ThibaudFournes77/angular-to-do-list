429e834a9afff80b6ecb75418d1f40c51f6a012c
Thibaud Fournès <thibaud.fournes@hotmail.fr>
2021-06-22 22:01:37 +0200

    Découpage de la to-do-list en plusieurs composants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TaskFormComponent } from './task-form/task-form.component';
+import { TasksListComponent } from './tasks-list/tasks-list.component';
+import { TaskComponent } from './task/task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskFormComponent,
+    TasksListComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule
