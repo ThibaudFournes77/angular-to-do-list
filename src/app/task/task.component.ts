22af32efc06d9f26c424e59a2f3804f2e29b4bea
Thibaud Fournès <thibaud.fournes@hotmail.fr>
2021-07-05 21:55:53 +0200

    propriétés personnalisées - attribution d'un libellé pour chaque tâche

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-task',
@@ -9,7 +9,7 @@ export class TaskComponent implements OnInit {
 
   constructor() { }
 
-  taskLibelle: string = "Publier un article sur Angular";
+  @Input() taskLibelle!: string;
 
   ngOnInit(): void {
   }
